47129ebe3f26bd2107cf4f0536bae96afd1690d9
Ubuntu <ubuntu@ip-172-31-12-151.ap-south-1.compute.internal>
2025-01-02 10:15:44 +0000

    Second commit

--- Tax_Age_Calculator.sh
+++ Tax_Age_Calculator.sh
@@ -3,7 +3,9 @@
 read -p "Enter salary: " salary
 if [ $salary -lt 30000 ]; then
     echo "No tax."
-elif [ $salary -le 40000 ]; then
+elif [ $salary -eq 30000 ]; then
+    echo "Tax: $(($salary * 5 / 100))"
+elif [ $salary -gt 30000 ] && [ $salary -le 40000 ]; then
     echo "Tax: $(($salary * 10 / 100))"
 else
     echo "Tax: $(($salary * 15 / 100))"
